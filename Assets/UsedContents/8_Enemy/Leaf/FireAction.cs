b2e54fc25ff5129dce16b6f8774fdffaaa68cffe
SatouTappei <stutpi.signup@gmail.com>
2023-06-07 12:09:42 +0900

    途中

--- Assets/UsedContents/8_Enemy/Leaf/FireAction.cs
+++ Assets/UsedContents/8_Enemy/Leaf/FireAction.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 /// <summary>
 /// 攻撃を行うアクションノード
 /// </summary>
@@ -8,6 +10,7 @@ public class FireAction : BehaviorTreeNode
 
     protected override void OnEnter()
     {
+        //var v = GameObject.FindGameObjectWithTag()
     }
 
     protected override void OnExit()
@@ -19,6 +22,16 @@ public class FireAction : BehaviorTreeNode
         BlackBoard.FireParticle.Play();
         TriggerByMonoBroker.Instance.AddShootData(ShootData.BulletType.Enemy, 
             BlackBoard.Transform.position, BlackBoard.Model.forward);
+        
+        // コライダーを発射する処理が必要
+        // TransformとModelへの参照は黒板にある
+        // Poolから弾を取り出して発射したい
+        // Poolへの参照どうするか問題
+        // このノードは使いまわしたい、コールバックを渡して実行するようにすれば近接攻撃にも対応できる？
+        // 案1: このクラスにプールをstaticで持たせる
+
+        // ★優先:攻撃をどうするか？このゲームには遠距離攻撃しか無い事は確定している。近接攻撃は考慮しないで良し
+
         return State.Success;
     }
 }
